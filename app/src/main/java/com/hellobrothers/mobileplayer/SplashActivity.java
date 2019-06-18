858b6b52a8eb98cc6e11c69291bbaf68b541d9be
hello-brothers <lh1479784191@qq.com>
2019-06-18 23:13:20 +0800

    first commit

---
+++ app/src/main/java/com/hellobrothers/mobileplayer/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.hellobrothers.mobileplayer;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //延迟2秒执行 handler在哪个线程创建就在哪个线程执行
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * 跳转到主界面
+     */
+    private void startMainActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //销毁handler 避免退出后还继续跳转到下一个界面
+        if (handler != null){
+            handler.removeCallbacksAndMessages(null);
+        }
+    }
+}
